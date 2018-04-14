b8e6c73a7d996d2afba3494f4dca7f79863f6b4d
twastwas <38160304+twastwas@users.noreply.github.com>
2018-04-14 17:39:41 -0400

    Update gol.js

--- sketches/gol.js
+++ sketches/gol.js
@@ -14,9 +14,9 @@ class Grid {
   constructor (cellSize) {
     // update the contructor to take cellSize as a parameter
     // use cellSize to calculate and assign values for numberOfColumns and numberOfRows
-	this.cellSize = cellSize;
-    this.numberOfColumns = height / cellSize;
-	this.numberOfRows = width / cellSize;
+	this.cellSize = cellSize; //has to access it on this
+    this.numberOfColumns = width / cellSize;
+	this.numberOfRows = height / cellSize;
 
 	this.cells = new Array(this.numberOfColumns); // create the initial array
 	for (var i = 0; i < this.cells.length; i ++) { // loop over each position in the array
@@ -25,19 +25,26 @@ class Grid {
 	
 	for (var column = 0; column < this.numberOfColumns; column ++) {
 		for (var row = 0; row < this.numberOfRows; row++) {
-			this.cells[column][row] = new Cell(column, row, cellSize)
+			this.cells[column][row] = new Cell(column, row, cellSize);
 		}
 	}
-	print(this.cells);
+	//print(this.cells);
+	print(this);
   }
 
   draw () {
     for (var column = 0; column < this.numberOfColumns; column ++) {
       for (var row = 0; row < this.numberOfRows; row++) {
-        fill(240); 
-        noStroke();
-        rect(column * this.cellSize + 1, row * this.cellSize + 1, this.cellSize - 1, this.cellSize - 1);
-      }
+        //fill(240); 
+        //noStroke();
+        //rect(column * this.cellSize + 1, row * this.cellSize + 1, this.cellSize - 1, this.cellSize - 1);
+      var currentCell = this.cells[column][row];
+	  
+	  currentCell.draw();
+	  //this.cells[column][row].draw();
+	  //??
+
+	  }
     }
   }
 }
@@ -48,5 +55,26 @@ class Cell{
 		this.row = row;
 		this.size = size;
 		this.isAlive = false;
+	}
+	
+	draw () {
+		
+		 if(this.isAlive == true) {
+			 fill(200,0,200);
+		 }
+		 else {
+			 fill(240);
+		//um
+		 }
+
+        noStroke();
+        rect(this.column * this.cellSize + 1, this.row * this.cellSize + 1, this.cellSize - 1, this.cellSize - 1);
+		//different color if alive or dead
+	}
+	
+	setIsAlive(value) {
+	
+///???
+	
 	}
 }
