ad8e759347bba78c66196cd3a2e73c3e371b1417
twastwas <38160304+twastwas@users.noreply.github.com>
2018-04-19 22:11:19 -0400

    Notes for next steps

--- sketches/gol.js
+++ sketches/gol.js
@@ -3,6 +3,22 @@ var grid;
 function setup () {
   createCanvas(400, 400);
   grid = new Grid(20);
+  
+  /*example
+  var x = 1;
+  var y = -1;
+  var sampleArray = [[0,0],[1,1]];
+ 
+  if (x >= 0 && x < sampleArray.length) {
+  //print(sampleArray[x][y]);
+  //do the same for y values
+  } else {
+	print("bad value"); //skip that cell
+  }
+  */
+    
+  //print(sampleArray[0][-1]);//[0,0], print(undefined)
+  //print(sampleArray[-1][0]);//print(undefined[0]);
 }
 
 function draw () {
@@ -26,6 +42,7 @@ class Grid {
 	for (var column = 0; column < this.numberOfColumns; column ++) {
 		for (var row = 0; row < this.numberOfRows; row++) {
 			this.cells[column][row] = new Cell(column, row, cellSize);
+			
 		}
 	}
 	//print(this.cells);
@@ -42,8 +59,6 @@ class Grid {
 	  
 	  currentCell.draw();
 	  //this.cells[column][row].draw();
-	  //??
-
 	  }
     }
   }
@@ -54,27 +69,40 @@ class Cell{
 		this.column = column;
 		this.row = row;
 		this.size = size;
-		this.isAlive = false;
+		this.isAlive = false; //we want this to keep updating
 	}
 	
 	draw () {
-		
-		 if(this.isAlive == true) {
-			 fill(200,0,200);
-		 }
+		 if(this.isAlive == false) {
+			 fill(200,0,200); //neighbor
+		//print("hi");		
+		}
 		 else {
-			 fill(240);
-		//um
+			 fill(240); //gray
 		 }
 
         noStroke();
-        rect(this.column * this.cellSize + 1, this.row * this.cellSize + 1, this.cellSize - 1, this.cellSize - 1);
+
+		//print(0);
+		//print("");
+		//print(); //this results in the window trying to print
+		//print(this.cellSize); //this results in the window trying to print
+		
+        rect(this.column * this.size + 1, this.row * this.size + 1, this.size - 1, this.size - 1);
 		//different color if alive or dead
 	}
 	
 	setIsAlive(value) {
-	
-///???
-	
+	//value is initially a number...
+	//create number then call the method
+	//converting that number to a boolean
+	//work on this...
 	}
+	
+	//counting the neighbor
+	//8 neighbors... squares - itself
+	
+	//3 neighbors don't exist =  only 5 neighbors
+	
+	//if else statement
 }
