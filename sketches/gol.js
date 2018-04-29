8322284a2df2cf9fd77be6e5b7249f9ba58c3614
twastwas <38160304+twastwas@users.noreply.github.com>
2018-04-29 17:52:56 -0400

    Final changes
    To make sure patterns are more pattern-like

--- sketches/gol.js
+++ sketches/gol.js
@@ -1,15 +1,14 @@
 var grid;
 
 function setup () {
-	createCanvas(200, 200);
-	frameRate(1);
-	grid = new Grid(5);
+	createCanvas(400, 400);
+	grid = new Grid(20);
 	grid.randomize();
 }
 
 function draw () {
 	background(250);
-	//grid.randomize();
+	//don't need randomize here
 	grid.updateNeighborCounts();
 	grid.updatePopulation();
 	grid.draw();
@@ -22,7 +21,7 @@ class Grid {
 		this.cellSize = cellSize; //has to access it on this
 		this.numberOfColumns = width / cellSize; //width = width of canvas
 		this.numberOfRows = height / cellSize; //height = height of canvas
-
+		
 		//creating the grid
 		this.cells = new Array(this.numberOfColumns); // create the initial array
 		for (var i = 0; i < this.cells.length; i ++) { // loop over each position in the array
@@ -35,8 +34,7 @@ class Grid {
 				this.cells[column][row] = new Cell(column, row, cellSize);
 			}
 		}
-		//print(this.cells);
-		//print(this);
+		//print wuz here
 	}
 
 	randomize() {
@@ -44,7 +42,7 @@ class Grid {
 			for (var row = 0; row < this.numberOfRows; row++) {
 				var currentCell = this.cells[column][row];
 				var randomNum = floor(random(2));
-				//print(randomNum); //always alternating 0 and 1..?? no the same values are just wrapped up together and the amount of the same values are listed in the value circled in blue
+				//print(randomNum); //always alternating 0 and 1..?? no the same values are just wrapped up together and the amount of the same values are listed in the value circled in blue on the inspect element's console tab
 				currentCell.setIsAlive(randomNum);
 			}
 		}
@@ -59,34 +57,39 @@ class Grid {
 				var currentCell = this.cells[column][row];
 				currentCell.liveNeighborCounts = 0;
 				
+				//going through neighboring cells
 				for (var xOffset = -1; xOffset <= 1; xOffset++) {
 					for (var yOffset = -1; yOffset <= 1; yOffset++) {
 						var neighborX = currentCell.column + xOffset;
 						var neighborY = currentCell.row + yOffset;
-						var amongtheliving = false;
-
+						var amongtheliving = false; //is the neighboring cell among the living? start with false
+						
+						//if x position of neighbor is less than 0 or greater than cells length, skip it cause it's not part of the grid 	
 						if(neighborX < 0 || neighborX >= this.numberOfColumns) {
 							neighborX = null;
 						}
+						//if y position of neighbor is less than 0 or greater than cells length, skip it cause it's not part of the grid 	
 						if(neighborY < 0 || neighborY >= this.numberOfRows) {
 							neighborY = null;
 						}
+						//check to see if "neigbor" has same coordinates as the currentCell- in that case it wouldn't be the neighbor, it would be cell itself so it won't be added to the neighbor count vv
 						if(currentCell.column == neighborX && currentCell.row == neighborY) {
-							//changed from offsetx and y to neighborx and y
+							//^^ changed from offsetx and y to neighborx and y
 							neighborX = null;
 							neighborY = null;
 						}
+						//now get the living state of neighbor
 						if(neighborX != null && neighborY != null) {
-						//if x and y position of neighbor is greater than 0 or less than cells length (so length is 20 but the it COUNTS 0-19), check it
+						
 							amongtheliving = this.cells[neighborX][neighborY].isAlive;
 						}
-						//if x or y position of neighbor is less than 0 or greater than cells length, skip it cause it's not part of the grid 	
+						//if living state is true, increase neighborcount of the current cell
 						if(amongtheliving == true) {
 							currentCell.liveNeighborCounts++;
-						}					
+						}
 					} 
 				}
-				//print(currentCell.liveNeighborCounts);
+				//print(this.cells[0][0].liveNeighborCounts);
 			}
 		}
 	}	
@@ -95,20 +98,18 @@ class Grid {
 		for (var column = 0; column < this.numberOfColumns; column ++) {
 			for (var row = 0; row < this.numberOfRows; row++) {
 				var currentCell = this.cells[column][row];
+				//print wuz here
 				currentCell.liveOrDie();
-			print(this.cells[0][1].liveNeighborCounts);
-			//off by one because it's maybe counting itself?
 			}
 		}  
 	}
 
 	draw() {
-    //go through each position to draw the cell
+    	//go through each position to draw the cell
 		for (var column = 0; column < this.numberOfColumns; column ++) {
 			for (var row = 0; row < this.numberOfRows; row++) {
 				var currentCell = this.cells[column][row];
 				currentCell.draw();
-				//this.cells[column][row].draw();
 				//print(this.cells[0][1].liveNeighborCounts);
 				//check the value to make sure it's actually doing what it needs to do
 			}
@@ -121,16 +122,16 @@ class Cell{
 		this.column = column;
 		this.row = row;
 		this.size = size;
-		this.isAlive; //we want this to keep updating...so I commented it out??
+		this.isAlive; //we want this to keep updating so don't assign it!
 		this.liveNeighborCounts = 0;
 	}
 
 	draw() {
 		if(this.isAlive == true) {
-			fill(200,0,200); //pink
+			fill(200,0,200); //pink = living
 		}
 		else {
-			fill(240); //gray
+			fill(240); //gray = dead
 		}
         noStroke();
 		//print(0); //print("");
@@ -140,61 +141,42 @@ class Cell{
 	}
 
 	setIsAlive(value) {
-		//this.value = value; //OMGGG this was the problem?! XD
-		//commented above out and got rid of this.value below cause Cell isn't supposed to have that as a parameter
 		if(value == 1) {
 			this.isAlive = true;
-			//print("hi" + 1);	
 		}
 		else {
-			this.isAlive = false;
-			//print("bi" + 0);	
+			this.isAlive = false;	
 		}
-		//print(this.value);
+		//print wuz here
 	}
 
-	/*
-	Any live cell with fewer than two live neighbours dies, as if caused by underpopulation.
-	Any live cell with two or three live neighbours lives on to the next generation.
-	Any live cell with more than three live neighbours dies, as if by overpopulation.
-	Any dead cell with exactly three live neighbours becomes a live cell, as if by reproduction.
-	*/
 	liveOrDie() {
  		var number = 0;
-		/*
-		if(this.isAlive == true) {
-			bornnext = false;
-		}
-		else if (this.liveNeighborCounts < 2 || this.liveNeighborCounts > 3) {
-			dienext = true;
-		}
-		else {
-			livenext = true;
-		}
-		*/
-	
+		//using number for logic can be confusing- proceed with caution
 		if(this.isAlive == false && this.liveNeighborCounts == 3) {
+			//"Any dead cell with exactly three live neighbours becomes a live cell, as if by reproduction." to me means +1
 			number = 1;
-			
 		}
 		else if(this.isAlive == true && (this.liveNeighborCounts < 2 || this.liveNeighborCounts > 3)) {
+			//"Any live cell with fewer than two live neighbours dies, as if caused by underpopulation."
+			//"Any live cell with more than three live neighbours dies, as if by overpopulation." to me means -1
 			number = -1;
 		}
 		else {
+		//"Any live cell with two or three live neighbours lives on to the next generation." to me means no change, or 0
 			number = 0;
 		}
-		//print(this);
-		//print(number);
-		
-		
+		//print wuz here
 		if(number == 1) {
+			//1 means death -> life
 			this.isAlive = true;
 		}
-		else if(number == 0) {
+		else if (number == -1) {
+			//-1 means life -> death
 			this.isAlive = false;
-		} 
-		else {
-			//do nothing
+		}
+		else{
+			//0 means life -> life or death -> death (no change)
 		}
 	}
 }
